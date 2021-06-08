2ca5fe9d249982c7eee5931da65dbca91b1f5a1e
Goyal Sandeep <null>
2021-06-08 12:16:26 +0530

    enable middleware parser for various type of post body

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyparser = require('body-parser'); //allow retrieving body for post routes
+
 const routeSoap = require('./routes/soapAPI.js');
 
 const app = express();
-const port = 1111;
+app.use(bodyparser.urlencoded({extended: true})); //Content-type: application/x-www-form-urlencoded
+app.use(bodyparser.json()); //Content-type: application/json
+app.use(bodyparser.raw()); //Content-type: text/plain
 
 app.get("/",(req,res)=> {
     console.log("request received");
@@ -11,6 +15,7 @@ app.get("/",(req,res)=> {
 
 app.use("/soap", routeSoap);
 
+const port = 1111;
 app.listen(port, function(){
     console.log("Server listening on port 1111");
 });
